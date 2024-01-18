a11df3531732cf754bf029c3f90a82b1a1621db3
Anik <crystal.nik@gmail.com>
2024-01-19 02:10:05 +0600

    json file added to asset folder, quote view composable added. code refactored

---
+++ app/src/main/java/com/example/quoteapp/DataManager.kt
@@ -0,0 +1,13 @@
+package com.example.quoteapp
+
+import android.content.Context
+import com.example.quoteapp.models.Quote
+
+object DataManager {
+
+    val data = emptyArray<Quote>()
+
+    fun readDataFromFile(context: Context) {
+
+    }
+}
